e711953753986a82959846215e781d5e26db9ee4
Benedikt Augenstein <benedikt.augenstein@googlemail.com>
2022-08-12 20:14:27 +0200

    update speech_recognition

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ from tensorflow.keras import models
 from tensorflow import keras
 from IPython import display
 from myfuncs import *
+import speech_recognition as sr
 from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
 model = keras.models.load_model('models/language-detection.h5')
+
+speech_engine = sr.Recognizer()
+
+languages = ['it', 'fr', 'de', 'en']
 
 app.secret_key='test'
 
@@ -33,7 +38,21 @@ def output():
     if request.method == 'POST':
         save_path = os.path.join("temp.wav")
         request.files['recorder'].save(save_path)
-        audio_file = request.files['recorder']
+        #audio_file = request.files['recorder']
+
+    with sr.WavFile(save_path) as source:  # use "test.wav" as the audio source
+        audio = speech_engine.record(source)
+        max_conf = 0
+        for ln in languages:
+            try:
+                conf = speech_engine.recognize_google(audio, language=ln)['alternative'][0]['confidence']
+            except:
+                conf = 0
+            if conf>max_conf:
+                max_conf = conf
+                max_ln = ln
+
+    """
     classes = ['Italian', 'French', 'German']
     AUTOTUNE = tf.data.AUTOTUNE
 
@@ -57,11 +76,14 @@ def output():
     result = str(result)
     second_result = '2. ' + preds2[1][1] + ' - Confidence: ' + str(round(preds2[1][0]*100, 2)) + '%'
     third_result = '3. ' + preds2[2][1] + ' - Confidence: ' + str(round(preds2[2][0]*100, 2)) + '%'
-
+    """
     finish = 'finished'
-
+    #result = str(preds)
     #result = str(save_path)
-    return render_template("input.html",result = result, second_result = second_result, third_result = third_result, finish=finish)
+    lang_dict = {'it':'Italian', 'fr':'French', 'de':'German', 'en':'English'}
+    lang_result = 'Detected language: ' + lang_dict[max_ln]
+    result = str(lang_result)
+    return render_template("input.html",result = result, finish=finish) #second_result = second_result, third_result = third_result,
 
 #if __name__ == '__main__':
     #app.run()
